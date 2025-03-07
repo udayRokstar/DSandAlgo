ee83b24821463c719d4a91b1f5fc0fea5edcd239
udayRockstar <udayhosamani5@gmail.com>
2025-03-07 06:40:50 +0530

    prime check

--- prime.js
+++ prime.js
@@ -1,16 +1,43 @@
-function isPrime(num) {
-    if (num <= 1) return false;
-    if (num <= 3) return true;
+// function isPrime(num) {
+//     if (num <= 1) return false;
+//     if (num <= 3) return true;
 
-    if (num % 2 === 0 || num % 3 === 0) return false;
+//     if (num % 2 === 0 || num % 3 === 0) return false;
 
-    for (let i = 5; i * i <= num; i += 6) {
-        if (num % i === 0 || num % (i + 2) === 0) return false;
-    }
+//     for (let i = 5; i * i <= num; i += 6) {
+//         if (num % i === 0 || num % (i + 2) === 0) return false;
+//     }
 
-    return true; 
-}
+//     return true; 
+// }
 
-// Example usage:
-console.log(isPrime(11)); // true
-console.log(isPrime(15)); // false 
+// // Example usage:
+// console.log(isPrime(37)); // true
+// // console.log(isPrime(7)); // false e
+
+
+
+// ---------------------------------------------------------------------
+
+// function myFunction(a) {
+//     for (let i = 2; i <= a - 1; i++) {
+//         if (a % i == 0) {
+//             console.log("Not Prime");
+//             return;
+//         }
+//     }
+//     console.log("Prime");
+//     return;
+// }
+
+// myFunction(0); // Prime
+
+// ---------------------------------------------------------------------
+
+const isPrime = num => {
+    for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+      if (num % i === 0) return false;
+    return num > 1; 
+  }
+
+  console.log(isPrime(37)); // true

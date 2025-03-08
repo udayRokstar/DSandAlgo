25dae790321f094c985aba88c44b489359a00171
udayRockstar <udayhosamani5@gmail.com>
2025-03-08 06:59:33 +0530

    Quick sort

--- insertionSort.js
+++ insertionSort.js
@@ -1,9 +1,21 @@
-function insertionSort(arr) {
-    for(let i=1; i<=arr.length; i++){
-        let se = arr[i];
-        let 
+function quickSort(arr) {
+    if(arr.length < 2) {
+        return arr
     }
-
+    let pivot = arr[arr.length-1];
+    let left = [];
+    let right = [];
+    for(let i=0; i<arr.length-1; i++){
+        if( arr[i] < pivot ) {
+            // left.push(arr[i]); // Assending order
+            right.push(arr[i]);  // Decending order
+        } else {
+            // right.push(arr[i])
+            left.push(arr[i])
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)]
 }
 
 const arr = [8, 20, -2, 4, -6];
+console.log(quickSort(arr));
